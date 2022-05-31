64726b9a14f6a46b98d87c8bf1315e531bf289ad
Kaup <ethan.kauf736@gmail.com>
2022-05-31 20:45:37 +0300

    1.4 - Mouse wheel

--- Main.cpp
+++ Main.cpp
@@ -4,90 +4,109 @@
 
 /*
 To do:
-flip-phone-9-keys keyboard
 scroll
+flip-phone-9-keys keyboard
 maybe the sound board?
+GUI
+Funny idea that is probably impossible: when starting the program the user's mouse will move to where the mouse in the exe icon at
 */
 
 void InputLoop()
 {
 	float m = 1;
-	float maxSpeed = 15.0f * m;
+	float wM = 1;
+
 	float acceleration = 1.0f * m;
+	float wAcceleration = 3.0f * wM;
+
+	float maxSpeed = 15.0f * m;
+	float wMaxSpeed = 25.0f * wM;
+
 	float brakeSpeed = 1.0f * m;
 	float brakeSpeedSingleAxis = 1.0f * m;
+	float wBrakeSpeed = 3.0f * wM;
+	
 	float xSpeed = 0.0f;
 	float ySpeed = 0.0f;
-	int xDes = 0;
-	int yDes = 0;
-
+	float wSpeed = 0.0f;
+	
 	int sizeofinput = sizeof(INPUT);
-	int microsecondPerTick = 10000;
+	int mcrosecsPerTick = 10000;
 	
 	bool lButton = false;
 	bool rButton = false;
 	bool mButton = false;
 	bool exit = false;
 
-	INPUT inputsDown[3];
-	INPUT inputsUp[3];
-
-	inputsDown[0].type = INPUT_MOUSE;
-	inputsDown[0].mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
-
-	inputsDown[1].type = INPUT_MOUSE;
-	inputsDown[1].mi.dwFlags = MOUSEEVENTF_RIGHTDOWN;
-
-	inputsDown[2].type = INPUT_MOUSE;
-	inputsDown[2].mi.dwFlags = MOUSEEVENTF_MIDDLEDOWN;
-
-	inputsUp[0].type = INPUT_MOUSE;
-	inputsUp[0].mi.dwFlags = MOUSEEVENTF_LEFTUP;
-
-	inputsUp[1].type = INPUT_MOUSE;
-	inputsUp[1].mi.dwFlags = MOUSEEVENTF_RIGHTUP;
+	INPUT mouseMove;
 	
-	inputsUp[2].type = INPUT_MOUSE;
-	inputsUp[2].mi.dwFlags = MOUSEEVENTF_MIDDLEUP;
+	INPUT mouseLDown;
+	INPUT mouseLUp;
+	
+	INPUT mouseRDown;
+	INPUT mouseRUp;
+	
+	INPUT mouseMidDown;
+	INPUT mouseMidUp;
 
-	inputsUp[0].mi.time = 0;
-	inputsUp[1].mi.time = 0;
-	inputsUp[2].mi.time = 0;
-	inputsDown[0].mi.time = 0;
-	inputsDown[1].mi.time = 0;
-	inputsDown[2].mi.time = 0;
+	INPUT mouseWheel;
+
+	mouseMove.type = INPUT_MOUSE;
+	mouseMove.mi.time = 0;
+	mouseMove.mi.dwFlags = MOUSEEVENTF_MOVE;
+
+	mouseLDown.type = INPUT_MOUSE;
+	mouseLDown.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+	mouseLDown.mi.time = 0;
 	
-	RegisterHotKey(NULL, 1, MOD_NOREPEAT, VK_NUMPAD5);
-	RegisterHotKey(NULL, 1, MOD_NOREPEAT, VK_NUMPAD8);
-	RegisterHotKey(NULL, 1, MOD_NOREPEAT, VK_NUMPAD6);
-	RegisterHotKey(NULL, 1, MOD_NOREPEAT, VK_NUMPAD4);
-	RegisterHotKey(NULL, 1, MOD_NOREPEAT, VK_NUMPAD7);
-	RegisterHotKey(NULL, 1, MOD_NOREPEAT, VK_NUMPAD9);
-	RegisterHotKey(NULL, 1, MOD_NOREPEAT, VK_NUMPAD2);
+	mouseLUp.type = INPUT_MOUSE;
+	mouseLUp.mi.dwFlags = MOUSEEVENTF_LEFTUP;
+	mouseLUp.mi.time = 0;
+
+	mouseRDown.type = INPUT_MOUSE;
+	mouseRDown.mi.dwFlags = MOUSEEVENTF_RIGHTDOWN;
+	mouseRDown.mi.time = 0;
+
+	mouseRUp.type = INPUT_MOUSE;
+	mouseRUp.mi.dwFlags = MOUSEEVENTF_RIGHTUP;
+	mouseRUp.mi.time = 0;
+
+	mouseMidDown.type = INPUT_MOUSE;
+	mouseMidDown.mi.dwFlags = MOUSEEVENTF_MIDDLEDOWN;
+	mouseMidDown.mi.time = 0;
+
+	mouseMidUp.type = INPUT_MOUSE;
+	mouseMidUp.mi.dwFlags = MOUSEEVENTF_MIDDLEUP;
+	mouseMidUp.mi.time = 0;
+
+	mouseWheel.type = INPUT_MOUSE;
+	mouseWheel.mi.dwFlags = MOUSEEVENTF_WHEEL;
+	mouseWheel.mi.time = 0;
+
+	RegisterHotKey(NULL, 0, MOD_NOREPEAT, VK_NUMPAD1);
+	RegisterHotKey(NULL, 0, MOD_NOREPEAT, VK_NUMPAD2);
+	RegisterHotKey(NULL, 0, MOD_NOREPEAT, VK_NUMPAD3);
+	RegisterHotKey(NULL, 0, MOD_NOREPEAT, VK_NUMPAD4);
+	RegisterHotKey(NULL, 0, MOD_NOREPEAT, VK_NUMPAD5);
+	RegisterHotKey(NULL, 0, MOD_NOREPEAT, VK_NUMPAD6);
+	RegisterHotKey(NULL, 0, MOD_NOREPEAT, VK_NUMPAD7);
+	RegisterHotKey(NULL, 0, MOD_NOREPEAT, VK_NUMPAD8);
+	RegisterHotKey(NULL, 0, MOD_NOREPEAT, VK_NUMPAD9);
+	RegisterHotKey(NULL, 0, MOD_NOREPEAT, VK_NUMPAD0);
 	
-	POINT MousePos;
-	
-	bool movedY;
-	bool movedX;
+	bool movedY = false;
+	bool movedX = false;
+	bool movedW = false;
 
 	auto start = std::chrono::high_resolution_clock::now();
 	auto end = std::chrono::high_resolution_clock::now();
-	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);;
 	auto exitStart = std::chrono::high_resolution_clock::now();
 	auto exitEnd = std::chrono::high_resolution_clock::now();
-
-	int timer = 500;
-
-	INPUT mouseInput;
-	mouseInput.type = INPUT_MOUSE;
-	mouseInput.mi.time = 0;
-	mouseInput.mi.dwFlags = MOUSEEVENTF_MOVE;
+	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
 	
 	while (true)
 	{
 		start = std::chrono::high_resolution_clock::now();
-		
-		timer--;
 
 		movedY = false;
 		movedX = false;
@@ -98,6 +117,8 @@ void InputLoop()
 			movedY = true;
 			if (ySpeed < maxSpeed)
 				ySpeed += acceleration;
+			if (ySpeed > maxSpeed)
+				ySpeed = maxSpeed;
 		}
 		// up
 		if (GetAsyncKeyState(VK_NUMPAD8))
@@ -105,6 +126,8 @@ void InputLoop()
 			movedY = true;
 			if (ySpeed > -maxSpeed)
 				ySpeed -= acceleration;
+			if (ySpeed < -maxSpeed)
+				ySpeed = -maxSpeed;
 		}
 		// right
 		if (GetAsyncKeyState(VK_NUMPAD6))
@@ -112,6 +135,8 @@ void InputLoop()
 			movedX = true;
 			if (xSpeed < maxSpeed)
 				xSpeed += acceleration;
+			if (xSpeed > maxSpeed)
+				xSpeed = maxSpeed;
 		}
 		// left
 		if (GetAsyncKeyState(VK_NUMPAD4))
@@ -119,44 +144,46 @@ void InputLoop()
 			movedX = true;
 			if (xSpeed > -maxSpeed)
 				xSpeed -= acceleration;
+			if (xSpeed < -maxSpeed)
+				xSpeed = -maxSpeed;
 		}
-		// lButton
+		// mouseL
 		if (GetAsyncKeyState(VK_NUMPAD7))
 		{
 			if (!lButton)
-				SendInput(1, &inputsDown[0], sizeofinput);
+				SendInput(1, &mouseLDown, sizeofinput);
 			lButton = true;
 		}
 		else
 		{
 			if (lButton)
-				SendInput(1, &inputsUp[0], sizeofinput);
+				SendInput(1, &mouseLUp, sizeofinput);
 			lButton = false;
 		}
-		// rButton
+		// mouseR
 		if (GetAsyncKeyState(VK_NUMPAD9))
 		{
 			if (!rButton)
-				SendInput(1, &inputsDown[1], sizeofinput);
+				SendInput(1, &mouseRDown, sizeofinput);
 			rButton = true;
 		}
 		else
 		{
 			if (rButton)
-				SendInput(1, &inputsUp[1], sizeofinput);
+				SendInput(1, &mouseRUp, sizeofinput);
 			rButton = false;
 		}
-		// mButton
+		// mouseM
 		if (GetAsyncKeyState(VK_NUMPAD2))
 		{
 			if (!mButton)
-				SendInput(1, &inputsDown[2], sizeofinput);
+				SendInput(1, &mouseMidDown, sizeofinput);
 			mButton = true;
 		}
 		else
 		{
 			if (mButton)
-				SendInput(1, &inputsUp[2], sizeofinput);
+				SendInput(1, &mouseMidUp, sizeofinput);
 			mButton = false;
 		}
 		// exit
@@ -179,6 +206,33 @@ void InputLoop()
 		else
 		{
 			exit = false;
+		}
+
+		// wUp
+		if (GetAsyncKeyState(VK_NUMPAD1))
+		{
+			if (wSpeed < wMaxSpeed)
+				wSpeed += wAcceleration;
+			if (wSpeed > wMaxSpeed)
+				wSpeed = wMaxSpeed;
+			movedW = true;
+		}
+		else
+		{
+			movedW = false;
+		}
+		// wDown
+		if (GetAsyncKeyState(VK_NUMPAD0))
+		{
+			if (wSpeed > -wMaxSpeed)
+				wSpeed -= wAcceleration;
+			if (wSpeed < -wMaxSpeed)
+				wSpeed = -wMaxSpeed;
+			movedW = true;
+		}
+		else if (!movedW) // lmao what a waste of time that was
+		{
+			movedW = false;
 		}
 		
 		if (!movedX)
@@ -223,29 +277,25 @@ void InputLoop()
 					ySpeed = 0;
 			}
 		}
+		if (!movedW)
+		{
+			if (wSpeed > wBrakeSpeed)
+				wSpeed -= wBrakeSpeed;
+			else if (wSpeed < -wBrakeSpeed)
+				wSpeed += wBrakeSpeed;
+			else
+				wSpeed = 0;
+		}
 
-		if (xSpeed > maxSpeed)
-			xSpeed = maxSpeed;
-		if (xSpeed < -maxSpeed)
-			xSpeed = -maxSpeed;
-
-		if (ySpeed > maxSpeed)
-			ySpeed = maxSpeed;
-		if (ySpeed < -maxSpeed)
-			ySpeed = -maxSpeed;
-
-		/*
-		GetCursorPos(&MousePos);
-		SetCursorPos(MousePos.x + (int)xSpeed, MousePos.y + (int)ySpeed);
-		GetCursorPos(&MousePos);
-		/*/
-		mouseInput.mi.dx = (int)xSpeed;
-		mouseInput.mi.dy = (int)ySpeed;
-		SendInput(1, &mouseInput, sizeofinput);
-		//*/
+		mouseMove.mi.dx = (long)xSpeed;
+		mouseMove.mi.dy = (long)ySpeed;
+		SendInput(1, &mouseMove, sizeofinput);
+		mouseWheel.mi.mouseData = (unsigned long)(wSpeed);
+		SendInput(1, &mouseWheel, sizeofinput);
+		
 		end = std::chrono::high_resolution_clock::now();
 		duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-		std::this_thread::sleep_for(std::chrono::microseconds(microsecondPerTick - duration.count()));
+		std::this_thread::sleep_for(std::chrono::microseconds(mcrosecsPerTick - duration.count()));
 	}
 }
 
